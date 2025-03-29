307727d99bc46722a99cde1884340c47e7758ab8
unknown <dscnaves@gmail.com>
2025-03-29 18:08:17 -0300

    Implementando funcionalidades dentro da classe Ambiente: mapa, imprimir mapa, atualizar posições dentro desse mapa para adicionar obstáculos ou robos, implementar função para verificar se uma posição do mapa está livre

--- lab02/Ambiente.java
+++ lab02/Ambiente.java
@@ -1,47 +1,80 @@
 import java.util.ArrayList;
 
-public class Ambiente{
-    // Atributos necessários para definir a classe Ambiente
+public class Ambiente {
     private int largura;
     private int altura;
     private int altitude;
     private ArrayList<Robo> robosAtivos;
+    private ArrayList<Obstaculo> obstaculos;
+    private String[][] mapa;
 
-    // Método construtor para inicialização dos atributos da classe Ambiente
-    public Ambiente(int altura, int largura){
+    public Ambiente(int altura, int largura) {
         this.largura = largura;
         this.altura = altura;
-        this.altitude = altitude;
+        this.altitude = 0;
         this.robosAtivos = new ArrayList<>();
+        this.obstaculos = new ArrayList<>();
+        this.mapa = new String[altura][largura];
+        inicializarMapa();
     }
 
-    // Método para adicionar robôs ao ambiente
+    private void inicializarMapa() {
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                mapa[i][j] = "_";
+            }
+        }
+    }
+
     public void adicionarRobo(Robo robo) {
-        robosAtivos.add(robo);
+        if (dentroDosLimites(robo.getPosicaoX(), robo.getPosicaoY(), 0)) {
+            robosAtivos.add(robo);
+            atualizarMapa(robo.getPosicaoX(), robo.getPosicaoY(), "&");
+        }
     }
 
-    // Função responsável por verificar se a posição de destino do robo está dentro dos limites do ambiente
-    public boolean dentroDosLimites(int x, int y, int z){
-        return ((x>=0 && x<=largura) && (y>=0 && y<=altura) && (z>=0 && z<altitude));
+    public void adicionarObstaculo(Obstaculo obstaculo) {
+        if (dentroDosLimites(obstaculo.getX(), obstaculo.getY(), 0)) {
+            obstaculos.add(obstaculo);
+            atualizarMapa(obstaculo.getX(), obstaculo.getY(), "*");
+        }
     }
-    
-    // Funções Getters e Setting
-    public void setAmbiente(int newLargura, int newAltura, int newAltitude){
+
+    private void atualizarMapa(int x, int y, String simbolo) {
+        if (x >= 0 && x < largura && y >= 0 && y < altura) {
+            mapa[y][x] = simbolo;
+        }
+    }
+
+    public boolean dentroDosLimites(int x, int y, int z) {
+        return ((x >= 0 && x < largura) && (y >= 0 && y < altura) && (z >= 0 && z <= altitude));
+    }
+
+    public boolean posicaoLivre(int x, int y) {
+        if (!dentroDosLimites(x, y, 0)) return false;
+        return mapa[y][x].equals("_");
+    }
+
+    public void exibirMapa() {
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                System.out.print(mapa[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Getters and Setters
+    public void setAmbiente(int newLargura, int newAltura, int newAltitude) {
         this.largura = newLargura;
         this.altura = newAltura;
         this.altitude = newAltitude;
+        this.mapa = new String[newAltura][newLargura];
+        inicializarMapa();
     }
 
-    public int getAmbienteLargura(){
-        return largura;
-    }
-
-    public int getAmbienteAltura(){
-        return altura;
-    }
-
-    public int getAmbienteAltitude(){
-        return altitude;
-    }
-
+    public int getAmbienteLargura() { return largura; }
+    public int getAmbienteAltura() { return altura; }
+    public int getAmbienteAltitude() { return altitude; }
+    public String[][] getMapa() { return mapa; }
 }
